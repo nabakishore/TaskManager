4952bd60eaf8766e62ef637543a68f00a1450524
nabakishore <nabakishore@gmail.com>
2019-06-03 23:26:24 +0530

    Changes after directory restructure

--- src/clnt.go
+++ src/clnt.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"./task"
+	"./sampletask"
+	"./taskengine"
 )
 
 
@@ -19,60 +20,9 @@ func Usage() {
 	fmt.Println("\t[exit] : exit Task Engine")
 }
 
-// Sample Task definitions
-type SampleTask task.Task 
-
-func (S *SampleTask) TRunning() bool {
-	if S.Status.TaskStarted && !S.Status.TaskExited {
-		return true
-	}
-
-	return false
-}
-
-func (S *SampleTask) TRun() {
-	S.Status.TaskStarted = true
-	for ; S.Status.TaskExit != true ; {
-		S.Status.TaskLoopCount++
-		time.Sleep(10 * time.Second)
-	} 
-	S.Status.TaskExited = true
-	fmt.Println("Exited SampleTask", S.TaskName)
-}
-
-func (S *SampleTask) TPause() {
-
-}
-
-func (S *SampleTask) TRestart() {
-
-}
-
-func (S *SampleTask) TStatus() {
-	fmt.Println(S.TaskName, "Running :", S.Status.TaskStarted && !S.Status.TaskExited)
-	fmt.Println("Task loop count : ", S.Status.TaskLoopCount)
-}
-
-func (S *SampleTask) TExit() {
-	S.Status.TaskExit = true
-	for ; S.Status.TaskStarted && S.Status.TaskExited != true ; {
-		time.Sleep(5 * time.Second)
-	}
-	S.Status.TaskStarted = false
-	S.Status.TaskExit = false
-	S.Status.TaskExited = false
-}
-
-func NewSampleTask(name string) *SampleTask {
-	return &SampleTask {
-		TaskId: name,
-		TaskName: name,
-	}
-}
-
 func main() {
 	var s string
-	var e *task.TaskEngine
+	var e *taskengine.TaskEngine
 
 
 	for ; s != "exit" ; {
@@ -85,13 +35,13 @@ func main() {
 			Usage()
 		case "start":
 			fmt.Println("Start task Manager")
-			e,_ = task.NewTaskEngine("Test Engine", 200)
+			e,_ = taskengine.NewTaskEngine("Test Engine", 200)
 		case "createtask":
 			var taskname string
-			fmt.Scanf("%s", &taskname)
-			sampletask := NewSampleTask(taskname)
 			var S task.TaskInterface
-			S = sampletask
+			fmt.Scanf("%s", &taskname)
+			t := sampletask.NewSampleTask(taskname)
+			S = t
 			e.AddTask(taskname, S)
 		case "deletetask":
 			var taskname string
--- src/task/task.go
+++ src/task/task.go
@@ -36,13 +36,6 @@ type Task struct {
         Status TaskStatus
 }
 
-type TaskEngine struct {
-	Name		string
-	TaskCount 	int
-	MaxTaskCount 	int
-	Tasks		map[string] TaskInterface
-}
-
 func (S *Task) TRinning() bool {
 	if S.Status.TaskStarted && !S.Status.TaskExited {
 		return true
@@ -77,75 +70,5 @@ func NewTask(name string) *Task {
 	return &Task {
 		TaskId: name,
 		TaskName: name,
-	}
-}
-
-func createTaskEngine(name string, maxcount int) *TaskEngine {
-	return &TaskEngine{
-		Name:	name,
-		TaskCount: 0,
-		MaxTaskCount: maxcount,
-		Tasks: make(map[string] TaskInterface),
-	}
-}
-
-// Start Task Engine
-func NewTaskEngine(name string, maxtask int) (*TaskEngine, error) {
-	eng := createTaskEngine(name, maxtask)
-
-	return eng, nil
-}
-
-// Exit Tasks in thas engine
-func (engine *TaskEngine) Exit() {
-	for _, el := range engine.Tasks {
-		el.TExit()	
-	}
-}
-
-// Add task to the task map
-func (engine *TaskEngine) AddTask(name string, task TaskInterface) error {
-	engine.Tasks[name] = task
-
-	return nil
-}
-
-// Remove task
-func (engine *TaskEngine) RemoveTask(name string) {
-	delete(engine.Tasks, name)
-}
-
-// Run a task
-func (engine *TaskEngine) RunTask(name string) error {
-	t := engine.Tasks[name]
-	var ti TaskInterface = t
-	if ti.TRunning() {
-		fmt.Println("Task already running")
-		return nil
-	}
-	go ti.TRun()
-	return nil
-}
-
-// Stop a task
-func (engine *TaskEngine) StopTask(name string) error {
-	el := engine.Tasks[name]
-	var ti TaskInterface = el
-	ti.TExit()
-	return nil
-}
-
-// Task Status
-func (engine *TaskEngine) TaskStatus(name string) error {
-	el := engine.Tasks[name]
-	var ti TaskInterface = el
-	ti.TStatus()
-	return nil
-}
-
-//List tasks
-func (engine *TaskEngine) ListTask() {
-	for key, _ := range engine.Tasks {
-		fmt.Println(key)
 	}
 }
